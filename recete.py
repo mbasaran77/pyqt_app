edb1658b45f01dd3797930cd6b845217bdd8cbb2
mbasaran77 <mbasaran77@gmail.com>
2017-01-06 13:53:19 +0300

    reçete edit devam

--- recete.py
+++ recete.py
@@ -4,6 +4,11 @@ import sys
 
 import pickle
 from pyModbusTCP import utils
+mydict = {0: ['renk 1', 0, 50], 1: ['renk 1', 60, 80], 2: ['renk 1', 120, 150], 3: ['renk 1', 160, 200]}
+
+
+
+den_list=[(0,10),(20,30),(50,85)]
 
 
 class recete_list(QDialog):
@@ -14,15 +19,23 @@ class recete_list(QDialog):
         self.list_w=QListWidget(self)
         self.btn_ekle=QPushButton("ekle",self)
         self.btn_sil=QPushButton("sil",self)
+        self.btn_edit=QPushButton("edit",self)
+
         self.txt_bas=QLineEdit(self)
         self.txt_son=QLineEdit(self)
 
         self.btn_ekle.clicked.connect(self.ekle)
         self.btn_sil.clicked.connect(self.sil)
+        self.btn_edit.clicked.connect(self.edit)
+        self.list_w.itemClicked.connect(self.list_w_click_item)
+
         self.initUi()
 
         #recete oluşturma
-        self.desen=color_list(self.color)
+
+        self.desen=color_list()
+        self.desen.setDict(mydict)
+        self.gun_liste()
 
 
     def initUi(self):
@@ -31,6 +44,7 @@ class recete_list(QDialog):
         lay_o_1.addWidget(self.txt_bas,0,0)
         lay_o_1.addWidget(self.txt_son, 0, 1)
         lay_o_1.addWidget(self.list_w, 1, 0)
+        lay_o_1.addWidget(self.btn_edit,1,1)
         lay_o_1.addWidget(self.btn_ekle, 2, 0)
         lay_o_1.addWidget(self.btn_sil, 2, 1)
         self.setLayout(lay_o_1)
@@ -38,74 +52,116 @@ class recete_list(QDialog):
     def ekle(self):
         bas=self.txt_bas.text()
         bit=self.txt_son.text()
-        self.desen.ekle(self.color,bas,bit)
+        self.desen.ekle(bas,bit)
         self.gun_liste()
+    def list_w_click_item(self):
+        index=self.list_w.currentRow()
+        item=self.list_w.currentItem()
+        print(index,item.text())
+        return (index,item.text())
+
+
+    def edit(self):
+        index,text=self.list_w_click_item()
+        self.desen.desen_kontrol()
+        print(index)
+
+
+        pass
 
     def sil(self):
-        pass
+        index,text=self.list_w_click_item()
+        self.desen.kaldir(index)
+        self.gun_liste()
 
     def gun_liste(self):
         self.list_w.clear()
         rec=self.desen.recete()
+        print("---------------------------------------")
+        print(rec)
+
+
         for i in range(len(rec)):
             txt=str(rec[i])
-            print(txt)
             self.list_w.addItem(txt)
-            print(rec[i])
 
 
 
-class color_list(object):
-    def __init__(self,color):
-        self.my_dict={}
-        self._id=0
-        self._bas=0
-        self._bit=0
-        self._color=color
-        self.my_dict_r={}
-    def setDict(self,sdict):
-        self.my_dict=sdict
-        self._id=len(self.my_dict)
+class color_list():
+        def __init__(self):
+            self.my_dict = {}
+            self.id = 0
+            self.my_dict_r = {}
 
-    def ekle(self,color,bas,bit):
-        self.my_dict[self._id]=[color,bas,bit]
-        self._id+=1
+        def setDict(self, sdict):
+            self.my_dict = sdict
+            self.id = len(self.my_dict)
 
-
-    def kaldir(self, id):
-        if len(self.my_dict)>=1:
-            self._id -= 1
-        self.my_dict.pop(id)
-
-        a=0
-        i=0
-        c=len(self.my_dict)
-        for i in range(len(self.my_dict)+1):
-            try:
-                self.my_dict_r[a]=self.my_dict[i]
-                a+=1
-            except KeyError:
+        def ekle(self, color, bas, bit):
+            max_l=self.desen_kontrol()
+            if max_l==0 or int(bas)>=max_l:
+                self.my_dict[self.id] = [color, bas, bit]
+                self.id += 1
+            elif bit(int)<max_l:
+                self.desen_uzat(color,bas,bit)
                 pass
-        i=0
-        a=0
-        self.my_dict.clear()
-        for i in range(len(self.my_dict_r)+1):
-           try:
-               self.my_dict[a]=self.my_dict_r[i]
-               a+=1
-           except KeyError:
-                pass
-        # print("my_dic_r ",self.my_dict_r)
-        self.my_dict_r.clear()
+
+        def desen_uzat(self,color,bas,bit):
+            fark=int(bit)-int(bas)
+            for a in self.my_dict:
+                renk, bas_r, son_r = self.my_dict[a]
+                if
 
 
-    def yazdir(self):
-        pass
-        # print("mydict  ",self.my_dict)
+            pass
 
-    def recete(self):
-        return self.my_dict
+        def kaldir(self, id):
+            if len(self.my_dict) >= 1:
+                self.id -= 1
+            self.my_dict.pop(id)
 
+            a = 0
+            i = 0
+            c = len(self.my_dict)
+            for i in range(len(self.my_dict) + 1):
+                try:
+                    self.my_dict_r[a] = self.my_dict[i]
+                    a += 1
+                except KeyError:
+                    pass
+            i = 0
+            a = 0
+            self.my_dict.clear()
+            for i in range(len(self.my_dict_r) + 1):
+                try:
+                    self.my_dict[a] = self.my_dict_r[i]
+                    a += 1
+                except KeyError:
+                    pass
+            # print("my_dic_r ",self.my_dict_r)
+            self.my_dict_r.clear()
+
+        def desen_kontrol(self):
+            if len(self.my_dict)>0:
+                max_l =0
+                for a in range(len(self.my_dict)):
+                    renk,bas,son=self.my_dict[a]
+                    if max_l<=int(son):
+                        max_l=int(son)
+            else:
+                max_l=0
+
+            return max_l
+
+
+
+
+        def yazdir(self):
+            pass
+            # print("mydict  ",self.my_dict)
+
+        def recete(self):
+            return self.my_dict
 
 
 class dosyakayit(object):
@@ -125,14 +181,6 @@ class dosyakayit(object):
             return oku_dict
 
 
-mydict = {0: ['renk1', 0, 500], 1: ['renk2', 50, 80], 2: ['renk3', 80, 120], 3: ['renk4', 120, 180],
-          4: ['renk5', 180, 220], 5: ['renk6', 220, 350],
-          6: ['renk1', 500, 900], 7: ['renk2', 90, 110], 8: ['renk3', 134, 560], 9: ['renk4', 455, 567],
-          10: ['renk5', 230, 420], 11: ['renk6', 304, 405], 11: ['renk1', 1000, 1200]}
-
-my_recete_dict = {0: ['Renk 1', '100.00', '250.4'], 1: ['Renk 2', '250.04', '500.00'],
-                  2: ['Renk 3', '500.00', '650.05'], 3: ['Renk 4', '650.05', '700'],
-                  4: ['Renk 1', '0.00', '50.00'], 5: ['Renk 2', '50', '200']}
 
 
 
