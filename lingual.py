873341e760f3d1a0740b9cb7b793813f734d0982
mbasaran77 <mbasaran77@gmail.com>
2018-03-24 15:26:47 +0300

    reçete dizi çalışması numpy

--- lingual.py
+++ lingual.py
@@ -2,10 +2,13 @@ import gettext
 
 _ = gettext.gettext
 
-tr = gettext.translation('lingual', localedir='locale', languages=['TR'])
-tr.install()
+tr_on = True
 
-_ = tr.gettext
+if tr_on:
+    tr = gettext.translation('lingual', localedir='locale', languages=['TR'])
+    tr.install()
+
+    _ = tr.gettext
 def print_some_strings():
     print(_("Hello world"))
     print(_("This is a translatable string"))
--- plc_recete_conv.py
+++ plc_recete_conv.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 recete_dict = (
 {0: [0, 100], 1: [800, 1000]},
 {0: [100, 200], 1: [1000, 1100]},
@@ -11,14 +13,49 @@ recete_dict = (
 
 #
 
-liste_renk = []
-i = 0
-for any_dict in recete_dict:
-    temp_list = []
-    for any_list in any_dict.values():
-        for val in any_list:
-            print(val)
-            temp_list.append(val)
-    liste_renk.append(temp_list)
+skala = 9.9
+off_set = [0, 60, 120, 180, 240, 300, 360, 420]
 
+def recete_dict_to_renk(dict_recete):
+    """
+    dict olarak verile receti alacak ve dizi döndürecek
+    :param dict_recete:
+    :return: renk_1, renk_2
+    """
+    sonuc = []
+    for any_dict in recete_dict:
+        temp_list = []
+        for any_list in any_dict.values():
+            for val in any_list:
+                temp_list.append(val)
+        sonuc.append(temp_list)
+    return sonuc
+
+liste_renk = recete_dict_to_renk(recete_dict)
 print(liste_renk)
+
+def recete_skala(list_renk, list_offset, skala):
+    a = np.array(list_renk)
+    sonuc = []
+    i = 0
+    for any_list in a:
+        b = any_list * skala + list_offset[i] * skala
+        print(b)
+        c = b.tolist()
+        sonuc.append(c)
+        i += 1
+    return sonuc
+s = recete_skala(liste_renk, off_set, skala)
+print(s)
+
+
+
+def recete_sirala(s_liste_renk):
+    a = np.array(s_liste_renk)
+    print(a)
+    b = a.flatten()
+    d = b.tolist()
+    e = d.sort()
+    print(e)
+
+recete_sirala(s)
